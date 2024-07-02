01b711a657ce4f39eeb313291027654a0c0d09a3
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-07-02 16:36:33 -0600

    almost got there

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,5 +1,21 @@
 function balancingParentheses(string) {
-  // type your code here
+    let missing = 0;
+    let openings = 0;
+  
+    for (let i = 0; i < string.length; ++i) {
+      if (string[i] === '(') {
+        ++openings;
+        continue;
+      }
+  
+      if (openings > 0) {
+        --openings;
+      } else {
+        ++missing;
+      }
+    }
+  
+    return missing + openings;
 }
 
 if (require.main === module) {
@@ -22,3 +38,8 @@ module.exports = balancingParentheses;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// function takes in string, and splits it into individual characters
+// those individual characters are put into an array, which is iterated over
+// increases count of variables to count opening and closing parenthesis
+// once the loop is done, subtracts the whichever number is smaller and returns that number
